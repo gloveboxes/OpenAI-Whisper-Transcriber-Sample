5d932b79d983acab07af2af7394c39045dd8dfd4
Dave Glover <dglover@microsoft.com>
2023-06-05 15:17:43 +1000

    Whisper MAUI Check in

---
+++ client/Whisper-MAUI/Whisper-MAUI/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Whisper_MAUI;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new WhisperNamespace.AudioPage();
+	}
+}
+
---
+++ client/Whisper-MAUI/Whisper-MAUI/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Whisper_MAUI;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
+
